23ac21d8526ffe2a26490f86a94421fac19c399d
Scott Perkins <scottrperkins96@gmail.com>
2023-05-03 19:21:31 +1200

    added ReactTypeAnimation thing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,18 +8,42 @@ import { TypeAnimation } from 'react-type-animation';
 
 
 const HomePage = () => {
-  const imageSize = useBreakpointValue({ base: '200px', md: '250px' });
-  const [textColor, setTextColor] = useState('red');
+  const imageSize = useBreakpointValue({ base: '250px', md: '400px' });
+  const [textColor, setTextColor] = useState('black');
 
   return (
     <PageTransition>
       <Box>
         <Flex direction={{ base: 'column', md: 'row' }} alignItems="center" justifyContent="space-between" minHeight="70vh" backgroundImage={bgImage} backgroundPosition="center"
           backgroundRepeat="no-repeat" backgroundSize="cover" py={16} px={{ base: 4, md: 12 }}>
-          <VStack spacing={4} alignItems="start" maxW="md" bgColor={useColorModeValue('white', 'gray.700')} borderRadius={"5%"} padding={"8"}> {/*Move this over slightly, and try and put a box around it so its visable with the bg image */}
+          <VStack spacing={4} alignItems="start" maxW="md" bgColor={useColorModeValue('white', 'gray.700')} borderRadius={"15%"} padding={"8"}> {/*Move this over slightly, and try and put a box around it so its visable with the bg image */}
             <Heading color={useColorModeValue('black', 'white')} mb={2}>
               Scott Perkins
             </Heading>
+            <>
+              <div
+                style={{
+                  fontSize: '25px', color: textColor, whiteSpace: 'pre-line'
+                  /* when working without ref and classNames, the manipulated style needs to be
+                   applied to the parent element, because the TypeAnimation component is perma-memoized */
+                }}>
+                <TypeAnimation
+                  sequence={[
+                    'Developer',
+                    800,
+                    () => setTextColor('black'),
+                    'Developer, creative',
+                    800,
+                    () => setTextColor('green'),
+                    'Developer, creative, student',
+                    1000,
+                    () => setTextColor('purple'),
+                    '',
+                  ]}
+                  repeat={Infinity}
+                />
+              </div>
+            </>
             <Text fontSize="lg" fontWeight="semibold" color={useColorModeValue('black', 'white')}>
               IT Student  & aspiring Software Developer
             </Text>
@@ -32,51 +56,6 @@ const HomePage = () => {
             <Button colorScheme="teal" mt={4} as={Link} to="/projects">
               View My Projects
             </Button>
-            <>
-    <div
-      style={{
-        fontSize: '35px',
-        color: textColor,
-        /* when working without ref and classNames, the manipulated style needs to be
-         applied to the parent element, because the TypeAnimation component is perma-memoized */
-      }}
-    >
-      <TypeAnimation
-        sequence={[
-          'One',
-          800,
-          () => setTextColor('aqua'),
-          'One Two',
-          800,
-          () => setTextColor('deeppink'),
-          'One Two Three',
-          1000,
-          () => setTextColor('darkkhaki'),
-          '',
-        ]}
-        repeat={Infinity}
-      />
-    </div>
-    <button
-      onClick={() => {
-        const items = [
-          'blue',
-          'green',
-          'purple',
-          'pink',
-          'brown',
-          'darkmagenta',
-          'darksalmon',
-          'dodgerblue',
-          'firebrick',
-          'darkviolet',
-        ];
-        setTextColor(items[Math.floor(Math.random() * items.length)]); // set random color
-      }}
-    >
-      Change Color
-    </button>
-  </>
           </VStack>
 
           <Image src={faceImage} alt="Scott Perkins" borderRadius="full" boxShadow="lg" objectFit="cover" width={imageSize} height={imageSize} mt={{ base: 8, md: 0 }} />
